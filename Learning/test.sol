d5f4c5672a92dab89cadba37e8131b663b101161
Devyush Raturi <devyushraturi@gmail.com>
2022-11-11 19:39:00 +0530

    Global Variables

--- Learning/test.sol
+++ Learning/test.sol
@@ -1,12 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 contract Mycontract{
-    function setter() public pure returns(int) {
-        int a = 10;
-        return a;
-    }
-    function setter2() public pure returns(int) {
-        int a = 20;
-        return a;
+    function global() public view returns(uint blockno, uint timestamp, address sender){
+        return (block.number, block.timestamp, msg.sender);
     }
 }
