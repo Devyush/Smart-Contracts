fb8db855529865fded8ebfd2d1faa3c56100ebf7
Devyush Raturi <devyushraturi@gmail.com>
2022-11-11 19:26:48 +0530

    Local Variable

--- Learning/test.sol
+++ Learning/test.sol
@@ -1,20 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 contract Mycontract{
-
-     //Declaring State Variables
-
-    //1st way: Assign the value of the variable in the same line it is declared.
-    //int public a = 10;
-
-    //2nd way: Assign the value of the variable through a contructor.
-    int public a;
-    // constructor() public {
-    //     a = 10;
-    // }
-
-    //3rd way: Assign the value of the variable through a function.
-    function setter() public {
-        a = a + 1;
+    function setter() public pure returns(int) {
+        int a = 10;
+        return a;
+    }
+    function setter2() public pure returns(int) {
+        int a = 20;
+        return a;
     }
 }
