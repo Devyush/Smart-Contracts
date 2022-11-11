3dd72752af26957044cc4f38b9ee498ca1cc0917
Devyush Raturi <devyushraturi@gmail.com>
2022-11-11 19:19:31 +0530

    state variables

--- Learning/test.sol
+++ Learning/test.sol
@@ -1,5 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 contract Mycontract{
-        
+
+     //Declaring State Variables
+
+    //1st way: Assign the value of the variable in the same line it is declared.
+    //int public a = 10;
+
+    //2nd way: Assign the value of the variable through a contructor.
+    int public a;
+    // constructor() public {
+    //     a = 10;
+    // }
+
+    //3rd way: Assign the value of the variable through a function.
+    function setter() public {
+        a = a + 1;
+    }
 }
