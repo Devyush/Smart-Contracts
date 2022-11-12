8286557006f8bdf4f4e090a0630ad3b2c76aac9d
Devyush Raturi <devyushraturi@gmail.com>
2022-11-12 11:25:07 +0530

    Structs

--- Learning/test.sol
+++ Learning/test.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 contract Mycontract{
-    function global() public view returns(uint blockno, uint timestamp, address sender){
-        return (block.number, block.timestamp, msg.sender);
+    struct Student{
+        uint age;
+        string name;
+    }
+    Student public s1;
+    constructor(){
+        s1.age=10;
+        s1.name="Diya";
+    }
+    function ChangeStruct() public{
+        s1.age=12;
+        s1.name="Mahesh";
     }
 }
